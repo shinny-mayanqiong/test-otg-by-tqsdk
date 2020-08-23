1a387a1b581c20927e3b2fc0ef7fa2f5177f98fe
mayanqiong <mayanqiong@shinnytech.com>
2020-08-23 17:51:55 +0800

    按交易所、日期下载盘后行情信息

--- md/md_test_not_on_trading_quote.py
+++ md/md_test_not_on_trading_quote.py
@@ -38,37 +38,47 @@ for i in range(5):
 def record_quotes(args):
     symbols, file_name, _stock, _md_url = args
     api = TqApi(auth=AUTH, _stock=_stock, _md_url=_md_url)
-    os.makedirs(f"quotes_not_on_trading_time", exist_ok=True)
-    csv_file = open(f"quotes_not_on_trading_time/{file_name}.csv", 'w', newline='')
+    os.makedirs(f"S:/mayanqiong/quotes_not_on_trading_time", exist_ok=True)
+    csv_file = open(f"S:/mayanqiong/quotes_not_on_trading_time/{file_name}", 'w', newline='')
     csv_writer = csv.writer(csv_file, dialect='excel')
     csv_writer.writerow(["symbol"] + QUOTE_KEYS)
-    for symbol in symbols:
+    group_size = 20
+    symbols_group = [symbols[i: i+group_size] for i in range(0, len(symbols), group_size)]
+    for one_symbols in symbols_group:
         api._send_chan.send_nowait({
             "aid": "subscribe_quote",
-            "ins_list": symbol
+            "ins_list": ",".join(one_symbols)
         })
         while True:
             api.wait_update()
-            quote = api._data.get("quotes", {}).get(symbol, {})
-            if quote and quote["datetime"] != "":
+            all_quotes_ready = True
+            quotes = api._data.get("quotes", {})
+            for s in one_symbols:
+                if quotes.get(s, {}).get("datetime", "") == "":
+                    all_quotes_ready = False
+                    break
+            if all_quotes_ready:
                 break
-        row = [symbol]
-        for k in QUOTE_KEYS:
-            row.append(quote.get(k))
-        csv_writer.writerow(row)
+        quotes = api._data.get("quotes", {})
+        for s in one_symbols:
+            quote = quotes.get(s)
+            row = [s]
+            for k in QUOTE_KEYS:
+                row.append(quote.get(k))
+            csv_writer.writerow(row)
     csv_file.close()
     api.close()
 
 if __name__ == '__main__':
     rsp = requests.get(url="https://openmd.shinnytech.com/t/md/symbols/latest.json", timeout=30)
-    symbols = [k for k,v in rsp.json().items() if v["exchange_id"] in EXCHANGE_LIST]  # 全部合约
-    symbols_group = {ex: [k for k, v in symbols.keys() if v["exchange_id"] == ex] for ex in EXCHANGE_LIST}  # 按交易所分组合约
+    symbols = rsp.json()
+    symbols_group = {ex: [k for k, v in symbols.items() if v["exchange_id"] == ex] for ex in EXCHANGE_LIST}  # 按交易所分组合约
     inputs = []
-    dt = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    dt = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     for ex, symbols in symbols_group.items():
         inputs.append((symbols, f"quotes_{dt}_{ex}_old.csv", False, "wss://u.shinnytech.com/t/md/front/mobile"))
         inputs.append((symbols, f"quotes_{dt}_{ex}_new.csv", True, "wss://api.shinnytech.com/t/nfmd/front/mobile"))
-    pool = multiprocessing.Pool(processes=32)
+    pool = multiprocessing.Pool(processes=14)
     pool_outputs = pool.map(record_quotes, inputs)
     pool.close()
     pool.join()
